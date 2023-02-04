3d53be395ec73a1934a82722cf0af2a03448b078
Warlock9527 <xiaochen9991@outlook.com>
2023-02-03 23:37:49 -0500

    Redux added
    Redux is a tool to modify the state.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
+import {Provider} from "react-redux";
+import store from 'react-redux'
 
 
 
@@ -11,6 +13,8 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>
 );
