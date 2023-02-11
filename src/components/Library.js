6407400bb420254cf461de338b664c3afb45332c
grui0502 <grui0992@gmail.com>
2023-02-11 10:57:44 -0500

    change1_2.11

--- src/components/Library.js
+++ src/components/Library.js
@@ -4,6 +4,7 @@ const Library = () => {
     const [gameIds, setGameIds] = useState([]);
     const [gamelist, setgamelist] = useState([]);
     const [commlist, setcommlist] = useState([]);
+    //const [searchValue, setSearchValue] = useState([]);
     useEffect(() => {
         fetch('/numofgames')
             .then(response => response.json())
@@ -20,64 +21,55 @@ const Library = () => {
             .then(data => setcommlist(data))
             .catch((error) => console.log("error"));
     };
-
     return (
-        <div style={{ display: 'flex', height: '100%' }}>
-            <div style={{ width: '20%', height: '100%', overflow: 'auto' }}>
-                <div>
-                    <div>
-                        {gameIds.map(id => (
-                            <button key={id} onClick={() => handleJump(id.g_id)}>
-                                game{id.g_id}
-                            </button>
-                        ))}
-                    </div>
-                </div>
+        <div id="container" style={{ width: '100%' }}>
+            <div id="header" style={{ backgroundColor: "#EEEEEE" }}>
+                <h1 style={{ marginBottom: 0 }}><center>search box</center></h1>
             </div>
-            <div style={{ width: '80%', height: '100%', position: 'relative' }}>
-                <div
-                    style={{
-                        position: 'absolution',
-                        left: '20%',
-                        top: 0,
-                        bottom: 0,
-                        width: '1px',
-                    }}
-                />
+            <div id="menu" style={{ backgroundColor: "#EEEEEE", height: "350px", width: "50%", float: "left" }}>
+                {gameIds.map(id => (
+                    <ul>
+                        <button key={id} onClick={() => handleJump(id.g_id)}>
+                            game{id._gid}
+                        </button>
+                    </ul>
+                ))}
+            </div>
+            <div id="content" style={{ backgroundColor: "#EEEEEE", height: "350px", width: "50%", float: "left" }}>
                 <center>
-                    <div>
-                        <ul>
-                            {
-                                gamelist.map((games)=>{
-                                    return (
-                                        <div key={games.g_id}>
-                                            <p>id:{games.g_id}</p>
-                                            <p>intro:{games.g_intro}</p>
-                                            <p>name:{games.g_name}</p>
-                                            <img src={require("..//game_images//game_"+games.g_id.toString()+".jpg")} style={{width: 200}}/>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </ul>
-                        <ul>
-                            {
-                                commlist.map((comments)=>{
-                                    return (
-                                        <div key={comments.g_id}>
-                                            <p>user id:{comments.u_id}</p>
-                                            <p>game id:{comments.g_id}</p>
-                                            <p>comment:{comments._comm}</p><p>rate:{comments._rate}</p>
-                                        </div>
-                                    )
-                                })
-                            }
-                        </ul>
-                    </div>
+                    <ul>
+                        {
+                            gamelist.map((games)=>{
+                                return (
+                                    <div key={games.g_id}>
+                                        <p>id:{games.g_id}</p>
+                                        <p>intro:{games.g_intro}</p>
+                                        <p>name:{games.g_name}</p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </ul>
+                    <ul>
+                        {
+                            commlist.map((comments)=>{
+                                return (
+                                    <div key={comments.g_id}>
+                                        <p>user id:{comments.u_id}</p>
+                                        <p>game id:{comments.g_id}</p>
+                                        <p>comment:{comments.comm_content}</p><p>rate:{comments._rate}</p>
+                                    </div>
+                                )
+                            })
+                        }
+                    </ul>
                 </center>
             </div>
+
         </div>
+
     );
-};
+}
+
 
 export default Library;
