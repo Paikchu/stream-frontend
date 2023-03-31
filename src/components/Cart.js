61919d6197538536d9526c2ae8d94463b072eeee
grui0502 <111545696+grui0502@users.noreply.github.com>
2023-03-30 22:52:50 -0400

    Library 3.29

---
+++ src/components/Cart.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+
+
+const CartList = () => {
+    const [CartList, setCartList] = useState([]);
+    const [CurrentPrice, setCurrentPrice] = useState(99.99);
+    const [Prices,setPrices] = useState([]);
+    const [currentg_id, setcurrentg_id] = useState(1);
+    useEffect(() => {
+        fetch('/getCartList/2')
+            .then(response => response.json())
+            .then(data => setCartList(data))
+        fetch('/getPrices')
+            .then(response => response.json())
+            .then(data => setPrices(data))
+    }, []);
+
+    const getPrice = cart_gid => {
+        fetch('/getGamePrice/cart_gid')
+            .then(response => response.json())
+            .then(data => setCurrentPrice(data))
+    }
+    const handlePurchase = cart_gid => {
+        getPrice(cart_gid);
+        fetch('/cart_purchase',{
+        method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ o_uid:2,o_gid:cart_gid,o_value: Prices[cart_gid]["g_price"] })
+    })
+            .then((response) => response.json())
+            .then((data) => {
+                const updatedCartList = CartList.filter(cart => cart.cart_gid !== cart_gid);
+                setCartList(updatedCartList);
+            })
+            .catch((error) => console.log("error"));
+    }
+
+    const handleDeleteCart = cart_gid => {
+        fetch('/delete_cart/2/'+cart_gid.toString())
+            .then((response) => response.json())
+            .then((data) => {
+                const updatedCartList = CartList.filter(cart => cart.cart_gid !== cart_gid);
+                setCartList(updatedCartList);
+            })
+            .catch((error) => console.log("error"));
+    }
+
+    return (
+        <div className="cart-list" style={{ overflowY: 'scroll', height: 'auto' }}>
+            <h1>My Cart</h1>
+            <table>
+                <thead>
+                <tr>
+                    <th>Game ID</th>
+                    <th>Game Name</th>
+                    <th>Money</th>
+                    <th>Game Tag</th>
+                    <th>Publisher</th>
+                </tr>
+                </thead>
+                <tbody>
+                {CartList.map(cart => (
+                    <tr key={cart.cart_gid}>
+                        <td>{cart.g_id}</td>
+                        <td>{cart.g_name}</td>
+                        <td>{cart.g_price}</td>
+                        <td>{cart.g_tag}</td>
+                        <td>{cart.g_cid}</td>
+                        <td>
+                            <button type="link" onClick={() => handleDeleteCart(cart.cart_gid)} className="link-button">Delete</button>
+                        </td>
+                        <td>
+                            <button type="link" onClick={() => handlePurchase(cart.cart_gid)} className="link-button">Purchase</button>
+                        </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default CartList;
--- src/index.js
+++ src/index.js
@@ -6,8 +6,10 @@ import {Provider} from "react-redux";
 import store from './store.js'
 import Library from './components/Library.js'
 import Order from './components/Order.js'
+import Cart from './components/Cart.js'
 import './css/library.css';
 import './css/GameOrderList.css';
+import './css/Cart.css';
 import './/Bootstrap/css/bootstrap.min.css';
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/antd/dist/antd.min.css" />
 
@@ -18,5 +20,5 @@ root.render(
             <Library />
         </Provider>
     </React.StrictMode>*/
-    <Order />
+    <Cart />
 );
